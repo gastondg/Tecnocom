37507f7bc4ce728cb319875d8241e96676dd743d
Gaston <gastonace1@gmail.com>
2019-09-20 12:02:01 -0300

    1080

--- test_cam.py
+++ test_cam.py
@@ -3,8 +3,8 @@ import numpy as np
 from datetime import datetime
 
 # Create a VideoCapture object
-cap = cv2.VideoCapture('rtsp://admin:tecno20@10.10.4.152:554/cam/realmonitor?channel=1&subtype=1')
-#cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture('rtsp://admin:tecno20@10.10.4.152:554/cam/realmonitor?channel=1&subtype=1')
+cap = cv2.VideoCapture("http://10.10.4.152/axis-cgi/mjpg/video.cgi?camera=1")
 
 
 # Check if camera opened successfully
@@ -13,14 +13,14 @@ if (cap.isOpened() == False):
 
 # Default resolutions of the frame are obtained.The default resolutions are system dependent.
 # We convert the resolutions from float to integer.
-#frame_width = int(cap.get(3))
-#frame_height = int(cap.get(4))
-
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+print(frame_width, frame_height)
 # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-""" out = cv2.VideoWriter('outpy152.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),\
-       20, (frame_width, frame_height)) """
-out = cv2.VideoWriter('outpy152-1080p.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),\
-                      15, (1920, 1080))
+out = cv2.VideoWriter('outpy152.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),\
+       20, (frame_width, frame_height)) 
+#out = cv2.VideoWriter('outpy152-1080p.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),\
+#                      15, (1920, 1080))
 
 # fecha inicio
 a = datetime.now()
