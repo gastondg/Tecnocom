ac902e2bf5338ecf6b197f3deb399177050b9bf6
Gaston <gastonace1@gmail.com>
2019-09-23 12:14:07 -0300

    for loop

--- test_cam.py
+++ test_cam.py
@@ -2,57 +2,63 @@ import cv2
 import numpy as np
 from datetime import datetime
 
-# Create a VideoCapture object
-#cap = cv2.VideoCapture('rtsp://admin:tecno20@10.10.4.152:554/cam/realmonitor?channel=1&subtype=1')
-cap = cv2.VideoCapture("http://10.10.4.151/axis-cgi/mjpg/video.cgi?camera=0")
+caps = ['rtsp://10.10.4.152:554/cam/realmonitor?channel=1&subtype=0&authbasic=YWRtaW46dGVjbm8yMA==',
+        'rtsp://10.10.4.151:554/cam/realmonitor?channel=1&subtype=0&authbasic=YWRtaW46dGVjbm8yMA==']
+
+filename = "outpy152.avi"
+for cap in caps:
+  # Create a VideoCapture object
+  cap = cv2.VideoCapture(cap)
+  #cap = cv2.VideoCapture("http://10.10.4.151/axis-cgi/mjpg/video.cgi?camera=0")
 
 
-# Check if camera opened successfully
-if (cap.isOpened() == False):
-  print("Unable to read camera feed")
+  # Check if camera opened successfully
+  if (cap.isOpened() == False):
+    print("Unable to read camera feed")
 
-# Default resolutions of the frame are obtained.The default resolutions are system dependent.
-# We convert the resolutions from float to integer.
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-print(frame_width, frame_height)
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-filename = "outpy151.avi"
-out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),\
-       15, (frame_width, frame_height)) 
-#out = cv2.VideoWriter('outpy152-1080p.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),\
-#                      15, (1920, 1080))
+  # Default resolutions of the frame are obtained.The default resolutions are system dependent.
+  # We convert the resolutions from float to integer.
+  frame_width = int(cap.get(3))
+  frame_height = int(cap.get(4))
+  print(frame_width, frame_height)
+  # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+  
+  out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),\
+        15, (frame_width, frame_height)) 
+  #out = cv2.VideoWriter('outpy152-1080p.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),\
+  #                      15, (1920, 1080))
 
-# fecha inicio
-a = datetime.now()
-while(True):
-  ret, frame = cap.read()
+  # fecha inicio
+  a = datetime.now()
+  while(True):
+    ret, frame = cap.read()
 
-  if ret == True:
+    if ret == True:
 
-    # Write the frame into the file 'output.avi'
-    out.write(frame)
+      # Write the frame into the file 'output.avi'
+      out.write(frame)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    
-    # controlo segundos pasados
-    b = datetime.now()
-    c = b-a
-    if int(c.seconds) == 25:
-      print(c.seconds)
+      # Display the resulting frame
+      cv2.imshow('frame', frame)
+      
+      # controlo segundos pasados
+      b = datetime.now()
+      c = b-a
+      if int(c.seconds) >= 25:
+        print(c.seconds)
+        break
+      # Press Q on keyboard to stop recording
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Break the loop
+    else:
       break
-    # Press Q on keyboard to stop recording
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
 
-  # Break the loop
-  else:
-    break
+  # When everything done, release the video capture and video write objects
+  cap.release()
+  filename = "outpy151.avi"
+  #out.release()
 
-# When everything done, release the video capture and video write objects
-cap.release()
-#out.release()
-
-# Closes all the frames
-cv2.destroyAllWindows()
+  # Closes all the frames
+  cv2.destroyAllWindows()
