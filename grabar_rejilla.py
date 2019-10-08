83f45c41db74d45c8621d8fb91f2a911bb614442
Gaston <gastonace1@gmail.com>
2019-10-08 15:44:18 -0300

    cambios de carpetas

---
+++ grabar_rejilla.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+import time
+
+"""  
+A TENER EN CUENTA: 
+- NO PONER PUNTOS
+- NO COLOCAR BARRAS
+
+Nombres válidos:
+2019-10-20 18:45 contaminacion pelets
+2019-11-05 08:30 contaminacion polvillo
+"""
+
+NOMBRE_VIDEO = "AAAA-MM-DD hh-mm nombre" + "{}.avi"
+
+
+def get_elapsed_seconds(start):
+  # devuelve el tiempo transcurrido, en segundos
+  return int(time.time() - start)
+
+cam_url = 'rtsp://10.10.4.151:554/cam/realmonitor?channel=1&subtype=0&authbasic=YWRtaW46dGVjbm8yMA=='
+
+cap = cv2.VideoCapture(cam_url)
+
+# Check if camera opened successfully
+if (cap.isOpened() == False):
+  print("No se pudo abrir la camara, revise la url de la cámara")
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+print("Empezando a grabar video...")
+
+out = get_nuevo_vid(filename.format(i), frame_width, frame_height)
+
+start = time.time()
+total_elapsed = time.time()
+
+i = 1
+
+while(get_elapsed_seconds(total_elapsed) < 43000):
+  ret, frame = cap.read()
+
+  if ret == True:
+
+    # Write the frame into the file 'output.avi'
+    out.write(frame)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    
+    if int(get_elapsed_seconds(start)) >= 10:
+      # pasaron 15 min -> grabo el video 
+      out.release()
+    
+      i += 1
+      out = get_nuevo_vid(filename.format(i), frame_width, frame_height)
+      start = time.time()
+
+    # Press Q on keyboard to stop recording
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+
+  # Break the loop
+  else:
+    break
+
+  
+
+# When everything done, release the video capture and video write objects
+cap.release()
+out.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
